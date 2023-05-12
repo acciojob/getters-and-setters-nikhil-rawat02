dd732617e6adda0c330f45fa59ef7093b53445e0
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-12 18:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
-//complete this code
+// //complete this code
+// class Person {
+// 	constructor(pName,pAge) {
+// 		this.name = pName;
+// 		this.age = pAge;
+// 	}
+// 	get name(){
+// 		return this.name;
+// 	}
+// 	set age(pAge){
+// 		this.age = pAge;
+// 	}
+// 	get age(){
+// 		return this.age;
+// 	}
+// }
+
+// class Student extends Person {
+// 	study(){
+// 		console.log(`${this.name} is studying`);
+// 	}
+// }
+
+// class Teacher extends Person {
+// 	teach(){
+// 		console.log(`${this.name} is teaching`);
+// 	}
+// }
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
+
 class Person {
-	constructor(pName,pAge) {
-		this.name = pName;
-		this.age = pAge;
-	}
-	get name(){
-		return this.name;
-	}
-	set age(pAge){
-		this.age = pAge;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
-
+ 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
+ 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-// Do not change the code below this line
+ 
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
